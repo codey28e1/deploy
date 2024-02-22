50169ae009ffdebdaf39335e8ab9d90b519dd4b1
Khiem Dinh <khiem7317@gmail.com>
2024-02-22 13:59:34 +0700

    kk

--- api/App.js
+++ api/App.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
         image: 'https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2019/7/1/741911/Hay-Trao-Cho-Anh-3.jpg'
     }).status(200)
 })
+app.get('/test', (req,res) => {
+    res.send({
+        msg: 'ok nha b ei'
+    }).status(200)
+})
 
 app.listen('4000', () => {
     console.log('App running on port 4000');
