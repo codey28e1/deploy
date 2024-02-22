ff13506a9f1efa7aec1bb8bf646c92a69a49af84
Khiem Dinh <khiem7317@gmail.com>
2024-02-22 18:02:08 +0700

    hhh

--- api/App.js
+++ api/App.js
@@ -4,14 +4,14 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send({
         name: 'Son Tung Mtp',
         position: 'Singer 1st vn',
         image: 'https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2019/7/1/741911/Hay-Trao-Cho-Anh-3.jpg'
     }).status(200)
 })
-app.get('/api/test', (req,res) => {
+app.get('/test', (req,res) => {
     res.send({
         msg: 'ok nha b ei'
     }).status(200)
